c66f02d33489661d70e9c6936eac99fed9b2e218
franco <x.francoruiz@gmail.com>
2022-01-06 02:35:49 -0300

    add loader for styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require("path");
 
+const ruleForStyles = {
+  test: /\.css$/,
+  use: ["style-loader", "css-loader"],
+};
+
+const ruleForJavascript = {
+  test: /\.js$/,
+  loader: "babel-loader",
+  options: {
+    presets: ["@babel/preset-react"],
+  },
+};
+
+const rules = [ruleForJavascript, ruleForStyles];
+
 module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-react"],
-        },
-      },
-    ],
+    rules,
   },
 };
